d1e490305effedae9e99a7c90881fd67953536d9
nathan-kennedy96 <nathanmkennedy96@gmail.com>
2024-09-02 12:11:43 -0700

    Update with Black Linting

--- python/build_commands.py
+++ python/build_commands.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 def update_python_commands() -> None:
     """
     Update the command.py python file from the contents of command.h
@@ -7,15 +8,15 @@ def update_python_commands() -> None:
 
     this_dir = Path(__file__).parent
     top_level_dir = this_dir.parent
-    command_header_path = top_level_dir/"stm32"/"Core"/"Inc"/"command.h"
+    command_header_path = top_level_dir / "stm32" / "Core" / "Inc" / "command.h"
 
     text_data = command_header_path.read_text()
-    #get text between {}
+    # get text between {}
     target_text = text_data.split("{")[-1].split("}")[0]
-    #no commas in the python implementation
+    # no commas in the python implementation
     fixed_text = target_text.replace(",", "")
 
-    #Write the text
+    # Write the text
     target_file = this_dir / "command.py"
     target_file_contents = f"""from enum import Enum
 
@@ -23,6 +24,7 @@ class Command(Enum):
     {fixed_text}
     """
     target_file.write_text(target_file_contents)
+
 
 if __name__ == "__main__":
     update_python_commands()
